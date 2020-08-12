166c8b3c2ad8eee2060f39512aac68f7feffa71c
Renan <RenanLopesAlmeida>
2020-08-12 17:34:43 -0300

    feature: creation of product information when expanded in order_screen

--- lib/src/screens/orders_screen/widgets/order_item.dart
+++ lib/src/screens/orders_screen/widgets/order_item.dart
@@ -1,11 +1,20 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:shop_app/src/models/order_model.dart';
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final OrderModel orderItem;
 
   const OrderItem({this.orderItem});
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  bool _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -14,15 +23,53 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text('\$ ${orderItem.amount}'),
+            title: Text('\$ ${widget.orderItem.amount.toStringAsFixed(2)}'),
             subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(orderItem.dateTime),
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.orderItem.dateTime),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon((!_expanded) ? Icons.expand_more : Icons.expand_less),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
-          )
+          ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: min((widget.orderItem.products.length * 20.0) + 30, 180),
+              color: Color(0xfff7f7f7),
+              child: ListView(
+                children: widget.orderItem.products
+                    .map(
+                      (product) => Container(
+                        margin: EdgeInsets.symmetric(vertical: 2),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              product.title,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              '${product.quantity} X \$${product.price}',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            )
         ],
       ),
     );
